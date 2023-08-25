b23b65cd8d611504fb63669b4e5071b57ce0fa9e
Diego <matadiego040@gmail.com>
2023-08-25 11:10:06 -0400

    jhjjh

--- Astra/sppp.js
+++ Astra/sppp.js
@@ -1,11 +1,16 @@
-let barras = [document.getElementById("")]
-let barras = [document.getElementById("")]
-let barras = [document.getElementById("")]
+
 
 let girar = true
-let men = document.getElementById("")
+let menu = document.getElementById("menu")
 
-for (let i = 0; i < barras.length; length; i++){
+let ver = true
+let Inicio = document.getElementById("Inicio")
+let Productos = document.getElementById("Productos")
+let Reservarsiones = document.getElementById("Reservar")
+let Contacto = document.getElementById("Contacto")
+
+
+for(let i = 0; i < barras.length; i++) {
     barras[i].addEventListener("click", () =>{
         menubar.style.transition = "0.5s"
         barras[i].style.transitio = "0.5s"
@@ -15,7 +20,6 @@ for (let i = 0; i < barras.length; length; i++){
             barras[1].style.opacity = "0"
             barras[0].style.transform= "rotate(45deg)" 
             barras[0].style.top="12px"
-            let barras: (HTMLElement | null)[] "(-45deg)"
             barras[0].style.transform= "rotate(-45deg)"
             barras[2].style.bottom= "15px"
             barras[2].style.left= "-1px"
@@ -23,5 +27,78 @@ for (let i = 0; i < barras.length; length; i++){
             menubar.style.zIndex = 2000
             menubar.style.opacity= 1
         }
-    })
-}
+        else{
+            barras[i].style.transition = "0.5s"
+            barras[1].style.opacity = "1"
+            barras[0].style.transform= "rotate(0deg)" 
+            barras[0].style.top="0px"
+            barras[0].style.transform= "rotate(0deg)"
+            barras[2].style.bottom= "0px"
+            barras[2].style.left= "0px"
+            girar = false 
+            menubar.style.zIndex = -100
+            menubar.style.opacity= 1   
+        }
+        Inicio.addEventListener("click", () => {
+         if (ver){
+                window.scroll (0,780)
+                barras[i].style.transition = "0.5s"
+                barras[1].style.opacity = "1"
+                barras[0].style.transform= "rotate(0deg)" 
+                barras[0].style.top="0px"
+                barras[0].style.transform= "rotate(0deg)"
+                barras[2].style.bottom= "0px"
+                barras[2].style.left= "0px"
+                girar = false 
+                menubar.style.zIndex = -100
+                menubar.style.opacity= 1     
+
+           
+            }})
+        Productos.addEventListener("click", () => {
+                if (ver){
+                       window.scroll (0,4000)
+                       barras[i].style.transition = "0.5s"
+                       barras[1].style.opacity = "1"
+                       barras[0].style.transform= "rotate(0deg)" 
+                       barras[0].style.top="0px"
+                       barras[0].style.transform= "rotate(0deg)"
+                       barras[2].style.bottom= "0px"
+                       barras[2].style.left= "0px"
+                       girar = false 
+                       menubar.style.zIndex = -100
+                       menubar.style.opacity= 1     
+       
+                  
+                   }})
+        Reservarsiones.addEventListener("click", () => {
+                    if (ver){
+                        window.scroll (0,900)
+                        barras[i].style.transition = "0.5s"
+                        barras[1].style.opacity = "1"
+                        barras[0].style.transform= "rotate(0deg)" 
+                        barras[0].style.top="0px"
+                        barras[0].style.transform= "rotate(0deg)"
+                        barras[2].style.bottom= "0px"
+                         barras[2].style.left= "0px"
+                        girar = false 
+                        menubar.style.zIndex = -100
+                        menubar.style.opacity= 1     
+           
+                      
+                    }})
+        Contacto.addEventListener("click", () => {
+                        if (ver){
+                        window.scroll (0,8000)
+                        barras[i].style.transition = "0.5s"
+                        barras[1].style.opacity = "1"
+                        barras[0].style.transform= "rotate(0deg)" 
+                        barras[0].style.top="0px"
+                        barras[0].style.transform= "rotate(0deg)"
+                        barras[2].style.bottom= "0px"
+                        barras[2].style.left= "0px"
+                        girar = false 
+                        menubar.style.zIndex = -100
+                        menubar.style.opacity= 1        
+                    }})
+    })}

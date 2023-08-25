06328c9f67e4fdd57f3c2ab87a1fcf77b4a4fcd4
Diego <matadiego040@gmail.com>
2023-08-25 09:05:52 -0400

    cambios

---
+++ Astra/sppp.js
@@ -0,0 +1,27 @@
+let barras = [document.getElementById("")]
+let barras = [document.getElementById("")]
+let barras = [document.getElementById("")]
+
+let girar = true
+let men = document.getElementById("")
+
+for (let i = 0; i < barras.length; length; i++){
+    barras[i].addEventListener("click", () =>{
+        menubar.style.transition = "0.5s"
+        barras[i].style.transitio = "0.5s"
+        barras[i].style.position = "Relative"
+        if(girar) {
+            barras[i].style.transition = "0.5s"
+            barras[1].style.opacity = "0"
+            barras[0].style.transform= "rotate(45deg)" 
+            barras[0].style.top="12px"
+            let barras: (HTMLElement | null)[] "(-45deg)"
+            barras[0].style.transform= "rotate(-45deg)"
+            barras[2].style.bottom= "15px"
+            barras[2].style.left= "-1px"
+            girar = false 
+            menubar.style.zIndex = 2000
+            menubar.style.opacity= 1
+        }
+    })
+}
